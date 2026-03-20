9c070f913a695224f3066eca8a4211f70ff0b8bd
PaulsGitH <paullydee0@gmail.com>
2026-03-20 15:40:39 +0000

    Signup resource

---
+++ lambdas/processImage.ts
@@ -0,0 +1,33 @@
+import { SQSHandler } from "aws-lambda";
+import {
+  GetObjectCommand,
+  GetObjectCommandInput,
+  S3Client,
+} from "@aws-sdk/client-s3";
+
+const s3 = new S3Client();
+
+export const handler: SQSHandler = async (event) => {
+  console.log("Event ", JSON.stringify(event));
+  for (const record of event.Records) {
+    const recordBody = JSON.parse(record.body);
+    if (recordBody.Records) {
+      console.log("Record body ", JSON.stringify(recordBody));
+      for (const messageRecord of recordBody.Records) {
+        const s3e = messageRecord.s3;
+        const srcBucket = s3e.bucket.name;
+        const srcKey = decodeURIComponent(s3e.object.key.replace(/\+/g, " "));
+        let theImage = null;
+        try {
+          const params: GetObjectCommandInput = {
+            Bucket: srcBucket,
+            Key: srcKey,
+          };
+          theImage = await s3.send(new GetObjectCommand(params));
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    }
+  }
+};
